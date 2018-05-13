0116f73b6b40182dc5c3f14621acf077c7b52fed
Paul Hoang <paul.hoang@hee.nhs.uk>
2018-05-14 00:02:03 +0100

    intial commit

---
+++ src/main/java/com/paulhoang/rebornserviceclientexample/RebornServiceClientExampleApplication.java
@@ -0,0 +1,12 @@
+package com.paulhoang.rebornserviceclientexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebornServiceClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebornServiceClientExampleApplication.class, args);
+	}
+}
