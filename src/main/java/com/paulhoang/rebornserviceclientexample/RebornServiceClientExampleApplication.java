c99e60a87ecbb1d06986f47da88407c434c4874a
General-Meow <spl.ivalis@gmail.com>
2018-05-14 22:13:00 +0100

    example of using eurka client to query the service discovery for another services homepage

--- src/main/java/com/paulhoang/rebornserviceclientexample/RebornServiceClientExampleApplication.java
+++ src/main/java/com/paulhoang/rebornserviceclientexample/RebornServiceClientExampleApplication.java
@@ -1,12 +1,32 @@
 package com.paulhoang.rebornserviceclientexample;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
+@EnableAutoConfiguration
 @SpringBootApplication
+@RestController
 public class RebornServiceClientExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RebornServiceClientExampleApplication.class, args);
+	}
+
+	@Autowired
+	private EurekaClient eurekaClient;
+
+	@GetMapping(path = "/test")
+	public String test() {
+		InstanceInfo nextServerFromEureka = eurekaClient.getNextServerFromEureka("REBORN SERVICE CLIENT EXAMPLE 2", false);
+		return "client 2 homepage: " + nextServerFromEureka.getHomePageUrl();
 	}
 }
