4bfed6f7436423648ba644e4cd5d14a8544f2136
General-Meow <spl.ivalis@gmail.com>
2018-08-02 23:18:22 +0100

    updated pom to use the new maven settings.xml for nexus

--- src/main/java/com/paulhoang/rebornserviceclientexample/RebornServiceClientExampleApplication.java
+++ src/main/java/com/paulhoang/rebornserviceclientexample/RebornServiceClientExampleApplication.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @EnableFeignClients
-@EnableAutoConfiguration
 @SpringBootApplication
 @RestController
 public class RebornServiceClientExampleApplication {
