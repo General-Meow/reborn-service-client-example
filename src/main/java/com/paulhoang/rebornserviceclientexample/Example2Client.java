4932261a5be08d7b66134ea85d745b5ca1df18b1
General-Meow <spl.ivalis@gmail.com>
2019-07-03 22:45:54 +0100

    added missing request type on the feign client

--- src/main/java/com/paulhoang/rebornserviceclientexample/Example2Client.java
+++ src/main/java/com/paulhoang/rebornserviceclientexample/Example2Client.java
@@ -2,10 +2,11 @@ package com.paulhoang.rebornserviceclientexample;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "REBORN-SERVICE-CLIENT-EXAMPLE-2", fallback = Example2ClientFallback.class)
 public interface Example2Client {
 
-  @RequestMapping("/hello")
+  @RequestMapping(value = "/hello", method = RequestMethod.GET)
   String getHello();
 }
