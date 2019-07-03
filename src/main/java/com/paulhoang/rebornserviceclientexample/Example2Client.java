29eeac59a529854eb81920d3eb36a79338544a8a
General-Meow <spl.ivalis@gmail.com>
2019-07-03 22:09:13 +0100

    change feign client name to use the all caps name used in service discovery app

--- src/main/java/com/paulhoang/rebornserviceclientexample/Example2Client.java
+++ src/main/java/com/paulhoang/rebornserviceclientexample/Example2Client.java
@@ -3,7 +3,7 @@ package com.paulhoang.rebornserviceclientexample;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "reborn-service-client-example-2", fallback = Example2ClientFallback.class)
+@FeignClient(name = "REBORN-SERVICE-CLIENT-EXAMPLE-2", fallback = Example2ClientFallback.class)
 public interface Example2Client {
 
   @RequestMapping("/hello")
