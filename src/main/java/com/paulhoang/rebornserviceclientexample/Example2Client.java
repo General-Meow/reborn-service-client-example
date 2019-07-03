f2ac92550070f0a601244f3e043541c21ffbae6e
General-Meow <spl.ivalis@gmail.com>
2019-07-03 22:51:21 +0100

    changed client back to lowercase

--- src/main/java/com/paulhoang/rebornserviceclientexample/Example2Client.java
+++ src/main/java/com/paulhoang/rebornserviceclientexample/Example2Client.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "REBORN-SERVICE-CLIENT-EXAMPLE-2", fallback = Example2ClientFallback.class)
+@FeignClient(name = "reborn-service-client-example-2", fallback = Example2ClientFallback.class)
 public interface Example2Client {
 
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
